72bb9049a46bd3338c0c29a27ec69e7b6ba3f5e0
César Silva <cesaraugusto90@gmail.com>
2021-09-12 10:56:10 -0300

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Cesar on 03/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
